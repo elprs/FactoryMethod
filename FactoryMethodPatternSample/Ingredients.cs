3fb380a1bd38bd08c7ddf8ccd91abec44a8149dd
Kostas Stroggylos <afdempkstr@users.noreply.github.com>
2020-04-08 19:39:28 +0300

    initial version

---
+++ FactoryMethodPatternSample/Ingredients.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternSample
+{
+    abstract class Ingredient
+    {
+        public abstract decimal Cost();
+    }
+
+    class Burger : Ingredient
+    {
+        public override decimal Cost()
+        {
+            return 2.0m;
+        }
+    }
+
+    class Cheese : Ingredient
+    {
+        public override decimal Cost()
+        {
+            return 0.8m;
+        }
+    }
+
+    class Lettuce : Ingredient
+    {
+        public override decimal Cost()
+        {
+            return 0.1m;
+        }
+    }
+
+    class Tzatziki : Ingredient
+    {
+        public override decimal Cost()
+        {
+            return 0.0m;
+        }
+    }
+
+    class Bread : Ingredient
+    {
+        public override decimal Cost()
+        {
+            return 0.5m;
+        }
+    }
+}
---
+++ FactoryMethodPatternSample/Sandwich.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternSample
+{
+    abstract class Sandwich
+    {
+        public List<Ingredient> Ingredients { get; }
+
+        public decimal TotalCost => Ingredients.Sum(ingredient => ingredient.Cost());
+
+        public Sandwich()
+        {
+            Ingredients = SelectIngredients();
+        }
+
+        protected abstract List<Ingredient> SelectIngredients();
+    }
+
+    class Souvlaki : Sandwich
+    {
+        protected override List<Ingredient> SelectIngredients()
+        {
+            return new List<Ingredient>
+            {
+                new Bread(),
+                new Burger(),
+                new Tzatziki()
+            };
+        }
+    }
+
+}
