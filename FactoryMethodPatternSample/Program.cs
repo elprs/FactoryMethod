81e115ce57a8b5f191a07efcacb5888411ec86d8
elprs <eleni.parisi.sunshine@gmail.com>
2020-04-09 10:14:07 +0300

    making sandwitches in main

--- FactoryMethodPatternSample/Program.cs
+++ FactoryMethodPatternSample/Program.cs
@@ -1,14 +1,35 @@
-﻿namespace FactoryMethodPatternSample
+﻿using System;
+
+namespace FactoryMethodPatternSample
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //SanswirchShop
+            Console.WriteLine("Where are you from?");
+            var country = Console.ReadLine();
+            var sanswitch = MakeSandwich(country.Trim());
+            // an thelo pros8eto kai alla ilika:
+            //sanswitch.Ingredients.Add(new Mustard());
+            Console.WriteLine($"Here, I made you a {sanswitch.GetType().Name}, it costs {sanswitch.TotalCost}.Thank you! ");
+            Console.WriteLine("=========================================");
             //FactoryMethod.FactoryMethodSample.Sample();
 
             AbstractFactory.AbstractFactoryDemo.Demo();
+
+            Console.ReadLine();
         }
 
-
+        static Sandwich MakeSandwich(string country)
+        {
+            switch (country)
+            {
+                case "USA":
+                    return new HotDog();
+                default:
+                    return new Souvlaki();
+            }
+        }
     }
 }
